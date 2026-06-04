7a94c2885e2d26dcc55094204bd8a30d7016c6f9
Sterry-Owl <benry16881217@gmail.com>
2026-06-04 09:58:27 +0800

    Fix config.js error

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 const peerConfig = { 
     config: { 
         'iceServers': [
-            { url: 'stun:stun.l.google.com:19302' }, 
-            { url: 'stun:stun1.l.google.com:19302' }
+            { urls: 'stun:stun.l.google.com:19302' }, 
+            { urls: 'stun:stun1.l.google.com:19302' }
         ] 
     } 
 };
