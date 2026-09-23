31d20ccd1bdc8c598aaa0b5244c9d25f90bab95c
Sterry-Owl <benry16881217@gmail.com>
2026-09-23 10:49:14 +0800

    修正拼字錯誤

--- js/config.js
+++ js/config.js
@@ -78,7 +78,7 @@ const NETWORK_CONFIG = {
 
 const TEMPLATES_TEST = [
     { id: "test-1_6", name: "介面測試", category: 'test', playerCount: 1, deck: ["預言家"] },
-    { id: "test_math", name: "雙機械狼測試", category: 'test', playerCount: 6, deck: ["通靈師", "女巫", "騎士", "守衛", "機械狼", "機械狼"] },
+    { id: "test_math", name: "雙機械狼測試", category: 'test', playerCount: 6, deck: ["魔鏡少女", "女巫", "騎士", "守衛", "機械狼", "機械狼"] },
 ];
 
 const TEMPLATES_FUN = [
